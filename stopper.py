dae558bcb72e4142cb80accc92795f578ca7bd17
Kevin Navero <knavero@gmail.com>
2018-02-08 02:26:28 -0800

    added range semantics through -s

--- stopper.py
+++ stopper.py
@@ -48,6 +48,24 @@ class Stopper(abc.Sequence):
          perc -= self._perc_to_dec(self._incrdelta)
       return stoplist
 
+   @classmethod
+   def get_stopper_within_support_range(
+      cls, support_range, highprice, numstops, fixed):
+      delta = 0.0
+      support_low = min(support_range)
+      support_high = max(support_range)
+      prev_calc = cls(support_low, highprice, delta, numstops, fixed)
+      calc = prev_calc
+
+      while True:
+         if calc[-1].price > support_high:
+            calc = prev_calc
+            break
+         prev_calc = calc
+         delta += 0.1
+         calc = cls(support_low, highprice, delta, numstops, fixed)
+      return calc
+
    def __init__(self, support, highprice, incrdelta, numstops, fixed=False):
       self._support = support
       self._highprice = highprice
@@ -69,30 +87,36 @@ class Stopper(abc.Sequence):
 
    @property
    def json(self):
-      return json.dumps({'Stop Losses Percent to Price': self._stoplist})
+      return json.dumps({
+         'Stop Losses Percent to Price': self._stoplist,
+         'IncrDelta percent': self._incrdelta }, sort_keys=True)
 
 def main():
    """stop loss calculator tests
 
    >>> sl = Stopper(49.34, 55.30, 5.00, 3)
    >>> sl.json
-   '{"Stop Losses Percent to Price": [[0.11, 49.21], [0.06, 51.98], [0.009999999999999995, 54.74]]}'
+   '{"IncrDelta percent": 5.0, "Stop Losses Percent to Price": [[0.11, 49.21], [0.06, 51.98], [0.009999999999999995, 54.74]]}'
 
    >>> sl = Stopper(49.34, 55.30, 1.00, 3)
    >>> sl.json
-   '{"Stop Losses Percent to Price": [[0.11, 49.21], [0.1, 49.77], [0.09000000000000001, 50.32]]}'
+   '{"IncrDelta percent": 1.0, "Stop Losses Percent to Price": [[0.11, 49.21], [0.1, 49.77], [0.09000000000000001, 50.32]]}'
 
    >>> sl = Stopper(49.34, 55.30, 0.5, Stopper.MAXNUMSTOP)
    >>> sl.json
-   '{"Stop Losses Percent to Price": [[0.11, 49.21], [0.105, 49.49], [0.09999999999999999, 49.77], [0.09499999999999999, 50.04], [0.08999999999999998, 50.32], [0.08499999999999998, 50.59], [0.07999999999999997, 50.87], [0.07499999999999997, 51.15], [0.06999999999999997, 51.42], [0.06499999999999996, 51.7], [0.05999999999999996, 51.98], [0.054999999999999966, 52.25], [0.04999999999999997, 52.53], [0.04499999999999997, 52.81], [0.03999999999999997, 53.08], [0.034999999999999976, 53.36], [0.029999999999999975, 53.64], [0.024999999999999974, 53.91], [0.019999999999999973, 54.19], [0.014999999999999972, 54.47], [0.00999999999999997, 54.74], [0.004999999999999971, 55.02]]}'
+   '{"IncrDelta percent": 0.5, "Stop Losses Percent to Price": [[0.11, 49.21], [0.105, 49.49], [0.09999999999999999, 49.77], [0.09499999999999999, 50.04], [0.08999999999999998, 50.32], [0.08499999999999998, 50.59], [0.07999999999999997, 50.87], [0.07499999999999997, 51.15], [0.06999999999999997, 51.42], [0.06499999999999996, 51.7], [0.05999999999999996, 51.98], [0.054999999999999966, 52.25], [0.04999999999999997, 52.53], [0.04499999999999997, 52.81], [0.03999999999999997, 53.08], [0.034999999999999976, 53.36], [0.029999999999999975, 53.64], [0.024999999999999974, 53.91], [0.019999999999999973, 54.19], [0.014999999999999972, 54.47], [0.00999999999999997, 54.74], [0.004999999999999971, 55.02]]}'
 
    >>> sl = Stopper(49.34, 55.30, 1.0, Stopper.MAXNUMSTOP)
    >>> sl.json
-   '{"Stop Losses Percent to Price": [[0.11, 49.21], [0.1, 49.77], [0.09000000000000001, 50.32], [0.08000000000000002, 50.87], [0.07000000000000002, 51.42], [0.06000000000000002, 51.98], [0.05000000000000002, 52.53], [0.040000000000000015, 53.08], [0.030000000000000013, 53.64], [0.02000000000000001, 54.19], [0.01000000000000001, 54.74]]}'
+   '{"IncrDelta percent": 1.0, "Stop Losses Percent to Price": [[0.11, 49.21], [0.1, 49.77], [0.09000000000000001, 50.32], [0.08000000000000002, 50.87], [0.07000000000000002, 51.42], [0.06000000000000002, 51.98], [0.05000000000000002, 52.53], [0.040000000000000015, 53.08], [0.030000000000000013, 53.64], [0.02000000000000001, 54.19], [0.01000000000000001, 54.74]]}'
 
    >>> sl = Stopper(51.26, 55.30, 1.0, 4, True)
    >>> sl.json
-   '{"Stop Losses Percent to Price": [[0.07305605786618444, 51.26], [0.06305605786618444, 51.81], [0.05305605786618444, 52.36], [0.04305605786618444, 52.91]]}'
+   '{"IncrDelta percent": 1.0, "Stop Losses Percent to Price": [[0.07305605786618444, 51.26], [0.06305605786618444, 51.81], [0.05305605786618444, 52.36], [0.04305605786618444, 52.91]]}'
+
+   >>> sl = Stopper.get_stopper_within_support_range((46.5, 48.321), 50, 4, True)
+   >>> sl.json
+   '{"IncrDelta percent": 1.2, "Stop Losses Percent to Price": [[0.07, 46.5], [0.05800000000000001, 47.1], [0.04600000000000001, 47.7], [0.034000000000000016, 48.3]]}'
    """
 
    parser = argparse.ArgumentParser(
@@ -103,26 +127,41 @@ def main():
          "$ python {} -s 49.34 -p 55.30 -n 3\n".format(sys.argv[0]) +
          "$ python {} -s 49.34 -p 55.30 -i 0.5\n".format(sys.argv[0]) +
          "$ python {} -s 49.34 -p 55.30\n".format(sys.argv[0]) +
-         "$ python {} -s 51.26 -p 55.30 -n 4 -f\n".format(sys.argv[0]))
-   parser.add_argument('-s', '--support', type=float, required=True, 
-      help="support price as a floating point value")
+         "$ python {} -s 51.26 -p 55.30 -n 4 -f\n".format(sys.argv[0]) +
+         "$ python3 {} -s 46.5 48.321 -p 50 -n 4 -f\n".format(sys.argv[0]))
+   parser.add_argument('-s', '--support', type=float, nargs='+', required=True,
+      help="support price(s) as floating point value(s)." +
+         " This may be two values specifying a range, for example " +
+         "-s 46.5 48.321. If specifying a range, -i cannot be used since " +
+         "range semantics will automatically adjust -i to the optimal value. " +
+         "Range semantics is the default. If only one value for -s is given, " +
+         "that value is used as the min and max of the range")
    parser.add_argument('-p', '--high-price', type=float, required=True,
       help="price of the security as a floating point value")
-   parser.add_argument('-i', '--incr-delta', type=float, default=1.0,
-      help="percentage between stops as a floating point value " +
-         "(defaults to 1.0 i.e. 1%%)")
+   parser.add_argument('-i', '--incr-delta', type=float,
+      help="percentage between stops as a floating point value." +
+         " This overrides range semantics specified in -s")
    parser.add_argument('-n', '--numstops', type=int, 
       default=Stopper.MAXNUMSTOP, 
       help="number of stops (as an int) to be calculated starting " +
          "from the support")
    parser.add_argument('-f', '--fixed', action='store_true',
-      help="don't perform a ceiling on the percentages")
+      help="don't perform a ceiling on the support percentage")
 
    args = parser.parse_args()
+   args.support = min(args.support), max(args.support)
 
-   calc = Stopper(
-      args.support, args.high_price, args.incr_delta, args.numstops,
-      args.fixed)
+   if args.incr_delta is not None:
+      calc = Stopper(
+         args.support[0], args.high_price, args.incr_delta, 
+         args.numstops, args.fixed)
+   elif args.numstops == Stopper.MAXNUMSTOP:
+      calc = Stopper(
+         args.support[0], args.high_price, 1.0, args.numstops, args.fixed)
+   else:
+      calc = Stopper.get_stopper_within_support_range(
+         args.support, args.high_price, args.numstops, args.fixed)
+
    print(calc.json)
 
 
